6adde79a0bd57464d8039c675bf9845d198f093d
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-06-03 16:56:10 -0300

    add_list_sort

--- Pokedex/Lista.c
+++ Pokedex/Lista.c
@@ -19,12 +19,13 @@ List *cria_lista(int size_dados, int size_refe){
     return li;
 
 }
-
+/*
 void * aux_add_list(void *dado, void *add, int size_dado){
     dado = malloc(size_dado);
 
     memcpy(dado, add, size_dado);
 }
+*/
 
 int add_list_sort(List *li, void *add_element, int *add_key, int size_refe){
 
@@ -32,10 +33,54 @@ int add_list_sort(List *li, void *add_element, int *add_key, int size_refe){
         return 0;
 
     No *no = (No *)malloc(sizeof(No));
+    no->ponteiro = (refe**) malloc(sizeof(refe*)*size_refe);
+
+    void *dado = malloc(li->size_dado);
+    memcpy(dado, add_element, li->size_dado);
+
+    no->data = dado;
 
 
+    for(int i = 0; i < size_refe; i++){
+        if(li -> size_list == 0){
+            no->ponteiro[i]->pos = NULL;
+            no->ponteiro[i]->pre = NULL;
+            li->first_no[i] = no;
+            li->last_no[i] = no;
+        }
+        else {
+
+            No *anterior, *atual = li -> first_no[i];
+
+            while(atual != NULL && atual->key[i] < add_key[i]){
+                anterior = atual;
+                atual = anterior->ponteiro[i]->pos;
+            }
+            if(atual == li -> first_no[i]){
+                no->ponteiro[i]->pre = NULL;
+                li->first_no[i]->ponteiro[i]->pre = no;
+                no->ponteiro[i]->pos = li->first_no[i];
+                li->first_no[i] = no;
+            } else {
+                no->ponteiro[i]->pos = anterior->ponteiro[i]->pos;
+                no->ponteiro[i]->pre = anterior;
+                anterior->ponteiro[i]->pos = no;
+
+                if(atual != NULL) {
+                    atual->ponteiro[i]->pre = no;
+                }
+                else{
+                    li->last_no[i] = no;
+                }
+            }
+        }
+        no->key[i] = add_key[i];
+        li -> size_list++;
+    }
+    return 1;
+}
+/*
     if(li -> size_list == 0){
-
         no -> pre = NULL;
         no -> pos = NULL;
         li -> first_no = no;
@@ -66,8 +111,9 @@ int add_list_sort(List *li, void *add_element, int *add_key, int size_refe){
     }
     no -> key = add_key;
     li -> size_list++;
-}
+*/
 
+/*
 void print_lista(List *li){
 
     No *no = li -> first_no;
@@ -80,3 +126,4 @@ void print_lista(List *li){
     }
 
 }
+*/
